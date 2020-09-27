bfd8065bca5c12388dfc0719677a33e5a9384189
River-Ye <river800111@gmail.com>
2020-09-27 14:02:20 +0800

    Add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 require 'resque/server'
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :orders
   resources :users
   mount Resque::Server.new, at: '/resque'
+  mount Sidekiq::Web => '/sidekiq'
   root 'users#index'
 end
