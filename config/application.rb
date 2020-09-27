01e4414d5bafad37e4bfa7a31a339a7c48722d3c
River-Ye <river800111@gmail.com>
2020-09-27 14:13:16 +0800

    將 ActiveJob 改 Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Ironman12th2020
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = "Taipei"
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
